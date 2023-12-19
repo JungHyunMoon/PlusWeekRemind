5544955753ab57817eab19572633590d9b04de23
문정현 <foqlzm12345@gmail.com>
2023-12-19 20:31:49 +0900

    init commit

---
+++ src/test/java/com/sparta/plusweekremind/PlusWeekRemindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.plusweekremind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlusWeekRemindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
