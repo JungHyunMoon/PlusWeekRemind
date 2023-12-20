42b5ca83d0a40b43b1f729add6dce6b510f78ef8
문정현 <foqlzm12345@gmail.com>
2023-12-20 21:57:03 +0900

    feat : spring security
    가입 유저 모드 USER Role로 회원가입

---
+++ src/main/java/com/sparta/plusweekremind/common/security/UserDetailsImpl.java
@@ -0,0 +1,62 @@
+package com.sparta.plusweekremind.common.security;
+
+import com.sparta.plusweekremind.entity.User;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final com.sparta.plusweekremind.entity.User user;
+
+    public UserDetailsImpl(com.sparta.plusweekremind.entity.User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        // 모든 사용자에게 기본적으로 "ROLE_USER" 권한을 부여
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/sparta/plusweekremind/common/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.sparta.plusweekremind.common.security;
+
+import com.sparta.plusweekremind.entity.User;
+import com.sparta.plusweekremind.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException("Not Found " + username));
+
+        return new UserDetailsImpl(user);
+    }
+}
