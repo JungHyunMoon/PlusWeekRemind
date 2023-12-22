3c881f5b10c526edef772d0aa034639b59ebe635
문정현 <foqlzm12345@gmail.com>
2023-12-22 17:21:50 +0900

    feat : cascade
    의존성 주입된 entity cascade추가

--- src/main/java/com/sparta/plusweekremind/board/entity/Board.java
+++ src/main/java/com/sparta/plusweekremind/board/entity/Board.java
@@ -1,8 +1,12 @@
 package com.sparta.plusweekremind.board.entity;
 
+import com.sparta.plusweekremind.comment.entity.Comment;
 import com.sparta.plusweekremind.common.model.Timestamped;
+import com.sparta.plusweekremind.like.entity.UserBoardLike;
 import com.sparta.plusweekremind.user.entity.User;
 import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -33,6 +37,12 @@ public class Board extends Timestamped {
 
     // 이미지 경로를 저장할 필드
     private String imagePath;
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserBoardLike> userBoardLikes = new ArrayList<>();
 
     public void update(Board updateBoard) {
         title = updateBoard.title;
--- src/main/java/com/sparta/plusweekremind/comment/entity/Comment.java
+++ src/main/java/com/sparta/plusweekremind/comment/entity/Comment.java
@@ -2,7 +2,9 @@ package com.sparta.plusweekremind.comment.entity;
 
 import com.sparta.plusweekremind.board.entity.Board;
 import com.sparta.plusweekremind.common.model.Timestamped;
+import com.sparta.plusweekremind.like.entity.UserCommentLike;
 import com.sparta.plusweekremind.user.entity.User;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,7 +12,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -43,6 +48,10 @@ public class Comment extends Timestamped {
     @Column
     @CreatedDate
     private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "comment", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserCommentLike> userCommentLikes = new ArrayList<>();
+
 
     public void updateComment(String commentContent) {
         this.commentContent = commentContent;
